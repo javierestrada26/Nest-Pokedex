d6f41520162ddd0033da57f6242bcf5528ee21d7
javierestrada26 <javiestrada14@hotmail.com>
2026-08-27 13:44:39 -0500

    feat: add CommonModule and ParseMongoIdPipe for improved structure and validation in Pokemon module

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -66,8 +66,11 @@ export class PokemonService {
   }
 
   async remove(id: string) {
-    const pokemon = await this.findOne(id);
-    await pokemon.deleteOne();
+    //const pokemon = await this.findOne(id);
+    //await pokemon.deleteOne();
+    //return {id};
+    const result = this.pokemonModel.findByIdAndDelete(id);
+    return result;
   }
 
 
