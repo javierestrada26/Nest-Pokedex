4370dac4b28d4a303925ef76eda355968d267c3b
javierestrada26 <javiestrada14@hotmail.com>
2026-08-28 14:03:39 -0500

    feat: add Joi validation schema for environment variables and integrate with ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,23 +5,31 @@ import { PokemonModule } from './pokemon/pokemon.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { EnvConfiguration } from './config/app.config';
+import { JoiValidationSchema } from './config/joi.validation';
 
 
 @Module({
   imports: [
 
     ConfigModule.forRoot({
-      load: [EnvConfiguration]
+      load: [EnvConfiguration],
+      validationSchema: JoiValidationSchema,
     }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname,'..','public'),
     }),
-    MongooseModule.forRoot(process.env.MONGODB!),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule], // no es estrictamente necesario si ConfigModule es global, pero no está de más
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get('mongodb'),
+      }),
+    }),
     PokemonModule,
     CommonModule,
-    SeedModule
+    SeedModule,
   ],
 })
 export class AppModule {}
