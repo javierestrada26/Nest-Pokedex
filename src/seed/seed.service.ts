99a2cec274740b584a2ce71d10bcb8f96323aa3a
javierestrada26 <javiestrada14@hotmail.com>
2026-08-28 11:21:41 -0500

    feat: update executeSeed method to fetch and insert all Pokémon data, improving seeding functionality

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -17,13 +17,23 @@ export class SeedService {
   ){}
 
   async executeSeed(){
-    const {data} = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=10')
-    data.results.forEach(async ({name,url})=>{
+
+    await this.pokemonModel.deleteMany({});
+
+    const {data} = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650')
+
+    const pokemonToInsert:{name:string, no:number}[] = [];
+
+    data.results.forEach(({name,url})=>{
       const segments = url.split('/');
       const no:number = +segments[segments.length - 2];
-      const pokemon = await this.pokemonModel.create({name, no})
-      console.log({name, no})
-    })
+      pokemonToInsert.push({name,no})
+    });
+
+    await this.pokemonModel.insertMany(pokemonToInsert);
+
+
+
     return 'Seed Executed';
   }
 }
